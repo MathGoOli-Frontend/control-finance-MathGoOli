fcb058ab26735783149c63592d6d9118e9b2bc7d
mathGoOli <matheuss456@hotmail.com>
2023-04-16 15:44:12 -0300

    html pronto

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,5 @@
 /* Desenvolva sua lógica aqui */
+import {handleModal, handleModalCloseButton} from './modal.js'
+
+handleModal()
+handleModalCloseButton()
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,22 @@
 /* Desenvolva sua lógica aqui */
+
+export function handleModal(){
+    const button = document.getElementsByClassName('header__button')[0]
+    const modal = document.querySelector('.register__dialog')
+    button.addEventListener('click',()=>{
+        
+        modal.showModal()
+    })
+}
+
+export function handleModalCloseButton(){
+    const modal = document.querySelector('.register__dialog')
+    const buttons = document.querySelectorAll('.register__close')
+    
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) =>{
+            event.preventDefault()
+            modal.close()
+        })
+    })
+}
